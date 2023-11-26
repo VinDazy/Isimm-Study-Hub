bbf6a702adaf1fdd61f26bc8f6b82176fb37486f
VinDazy <hectorvalinci@gmail.com>
2023-11-26 16:52:36 +0100

    debug image

--- functions.py
+++ functions.py
@@ -181,7 +181,7 @@ def display_subfolder_links(folder_links):
                 folder_name = folder_name[:-1]
             image_path = f'media//subject_logos//{folder_name}.png'
             st.markdown(f"<b>Subject : </b> {folder_name}", unsafe_allow_html=True)
-            st.image(image_path, width=350, caption="", use_column_width=False)  # Displaying the image
+            #st.image(image_path, width=350, caption="", use_column_width=False)  # Displaying the image
             st.markdown(f"📁 [Visit Folder]({folder_link})")  # Displaying the link
             st.markdown("---")
 
