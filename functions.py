60bb4e247f95e5668fea2c4ea0e24e6777325053
VinDazy <hectorvalinci@gmail.com>
2023-11-30 20:50:58 +0100

    create service functions

--- functions.py
+++ functions.py
@@ -15,12 +15,12 @@ from urllib.parse import urljoin
 
 
 def Create_Service(client_secret_file, api_name, api_version, *scopes):
-    #print(client_secret_file, api_name, api_version, scopes, sep='-')
+    print(client_secret_file, api_name, api_version, scopes, sep='-')
     CLIENT_SECRET_FILE = client_secret_file
     API_SERVICE_NAME = api_name
     API_VERSION = api_version
     SCOPES = [scope for scope in scopes[0]]
-    #print(SCOPES)
+    print(SCOPES)
 
     cred = None
 
@@ -43,7 +43,7 @@ def Create_Service(client_secret_file, api_name, api_version, *scopes):
 
     try:
         service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
-        #print(API_SERVICE_NAME, 'service created successfully')
+        print(API_SERVICE_NAME, 'service created successfully')
         return service
     except Exception as e:
         print('Unable to connect.')
