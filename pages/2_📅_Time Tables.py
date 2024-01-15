8773462fadd08b16d7be21c12e275cce02c48c6f
VinDazy <hectorvalinci@gmail.com>
2024-01-15 21:23:50 +0100

    updating semester 2 timetables

--- pages/2_📅_Time Tables.py
+++ pages/2_📅_Time Tables.py
@@ -15,10 +15,12 @@ img,bg=st.columns([0.2,0.8],gap="large")
 
 img.image("media/isimm logo/isimm logo _ 20.png", width=270)
 bg.image("media/banner.jpeg", use_column_width=True)
+with st.sidebar:
+    semester=st.selectbox(label="Chose a Semester" ,options=["Semester 1","Semester 2"])
 tdGroups=["TD1","TD2","TD3","TD4","TD5"]
 group=st.selectbox("Please choose your TD group ",options=tdGroups)
-filePath=r"resources/timeTables/"+group+".png"
-image=st.image(image=filePath)
+filePath=f"resources/timeTables/{semester}/"+group+".png"
+image=st.image(image=filePath,use_column_width=True)
 st.toast(f" Displaying {group[:-1]+' '+group[-1]} Time Table",icon="🙌")
 
 col1,col2,col3,col4,col5=st.columns(5)
